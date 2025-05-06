768c600c9c6ab650f8cd2e4ca06b1bd1a7a7e397
Taishi Hashimoto <hashimoto.taishi@nipr.ac.jp>
2025-05-06 02:58:28 +0000

    test: added some notes

--- test/simple.py
+++ test/simple.py
@@ -8,25 +8,32 @@ from bpadmm.reference import basis_pursuit_admm as bpadmm_python
 from os.path import join, dirname
 
 
-N = 100
-M = 50
-K = 10
-A = np.random.normal(size=(M, N))
-x0 = np.vstack((np.random.normal(size=(K, 1)), np.zeros(shape=(N-K, 1))))
+N = 100  # The numner of parameters.
+M = 50  # The number of observations.
+K = 10  # The number of non-zero elements.
+
+A = np.random.normal(size=(M, N))  # Observation matrix.
+
+# Original signal.
+x0 = np.r_[np.random.normal(size=K), np.zeros(shape=N - K)]
 x0 = np.random.permutation(x0)
 
+# Measurement.
 y = A @ x0
 # %%
 
-threshold = np.linalg.norm(A) * 0.001
+threshold = np.linalg.norm(A) * 0.001  # Soft threashold.
 
 x, info = basis_pursuit_admm(A+0j, y+0j, threshold=threshold)
 # NOTE: xtol is set to very small value because basis_pursuit_admm doesn't
 #       check it.
 trace = bpadmm_python(A+0j, y+0j, threshold=threshold, trace=True, xtol=1e-16)
 
-# Check if results from Rust and Python coinsides.
-assert np.mean((x.ravel() - trace["x"][:, -1])**2) < 1e-6
+# Check if results from JAX and Python coinsides.
+assert np.mean((x - trace["x"][:, -1])**2) < 1e-6
+
+# Check if results from JAX recovered the original signal.
+assert np.mean((x - x0)**2) < 1e-6
 
 fig, ax = plt.subplots()
 ax.stem(x0.real, linefmt="k-", markerfmt="ko")
@@ -36,7 +43,7 @@ ax.stem(x.real, linefmt="none", markerfmt="rx")
 fig.tight_layout()
 fig.savefig(join(dirname(__file__), "simple.png"))
 
-# %%
+# %% Check plot how computation went.
 
 x_nonzero = np.count_nonzero(np.abs(trace["x"]) > 1e-3, axis=0)
 z_nonzero = np.count_nonzero(np.abs(trace["z"]) > 1e-3, axis=0)
